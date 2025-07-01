d2d878ca8e65d059aac4cdc9fa5284e34cfaf981
Naldy <mdavindarinaldy@gmail.com>
2025-07-01 23:26:03 +0700

    feat: add struct for register and login in dto folder

---
+++ dto/auth.go
@@ -0,0 +1,14 @@
+package dto
+
+type AuthLogin struct {
+	Email    string `form:"email" json:"email" db:"email" binding:"required,email"`
+	Password string `form:"password" json:"password" db:"password" binding:"required"`
+}
+
+type AuthRegister struct {
+	Name            string `form:"name" json:"name" db:"name" binding:"required"`
+	Email           string `form:"email" json:"email" db:"email" binding:"required,email"`
+	PhoneNumber     string `form:"phoneNumber" json:"phoneNumber" db:"phone_number" binding:"required"`
+	Password        string `form:"password" json:"password" db:"password" binding:"required"`
+	ConfirmPassword string `form:"confPass" json:"confPass" binding:"required"`
+}
