fa091fed4c5ece6a9f20f5387b16f843b322a5b5
Naldy <mdavindarinaldy@gmail.com>
2025-07-02 18:02:43 +0700

    chore: set up database (add query to insert, select and delete rows)

--- init.sql
+++ init.sql
@@ -1,91 +1,77 @@
-CREATE TYPE role AS ENUM ('user','admin');
+INSERT INTO users (name, email, phone_number, password, role, created_at)
+VALUES
+('admin','admin@gmail.com','0812345','admin123','admin','2025-07-02');
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    phone_number VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role role NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT NOW()
-);
+SELECT m.title, m.synopsis, m.release_date, m.price, m.runtime, m.poster, m.backdrop FROM movies m
+JOIN movies_genres mg ON mg.id_movie = m.id
+JOIN genres g ON g.id = mg.id_genre
+WHERE m.title ILIKE '%a%' AND g.name ILIKE '%Adventure%';
 
-CREATE TABLE movies (
-    id SERIAL PRIMARY KEY,
-    created_by INT REFERENCES users(id),
-    title VARCHAR(255) NOT NULL,
-    synopsis TEXT NOT NULL,
-    release_date DATE NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    runtime INT NOT NULL,
-    poster VARCHAR(255) NOT NULL,
-    backdrop VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT NOW()
-);
+DELETE FROM directors WHERE id=24;
 
-CREATE TABLE genres (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE, 
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT NOW()
-);
+DELETE FROM movies_casts WHERE id_movie>0;
 
-CREATE TABLE directors (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE, 
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT NOW()
-);
+INSERT INTO movies_directors (id_director, id_movie)
+VALUES
+(1,1),
+(2,2),
+(3,3),
+(4,3),
+(5,4),
+(6,5),
+(7,6),
+(8,7),
+(9,8),
+(10,9),
+(11,10),
+(12,11),
+(13,12),
+(14,13),
+(15,13),
+(16,14),
+(17,15),
+(18,16),
+(19,17),
+(20,18),
+(21,19),
+(22,20),
+(23,20);
 
-CREATE TABLE casts (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE, 
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE movies_genres (
-    id_genre INT REFERENCES genres(id),
-    id_movies INT REFERENCES movies(id),
-    created_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE movies_directors (
-    id_director INT REFERENCES directors(id),
-    id_movies INT REFERENCES movies(id),
-    created_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE movies_casts (
-    id_cast INT REFERENCES casts(id),
-    id_movies INT REFERENCES movies(id),
-    created_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE payment_methods (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE transactions (
-    id SERIAL PRIMARY KEY,
-    id_user INT REFERENCES users(id),
-    id_movie INT REFERENCES movies(id),
-    id_payment_method INT REFERENCES payment_methods(id),
-    total_amount DECIMAL(10,2) NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    cinema VARCHAR(255) NOT NULL,
-    showtime DATE NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE transactions_detail (
-    id SERIAL PRIMARY KEY,
-    id_transaction INT REFERENCES transactions(id),
-    seat VARCHAR NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
+INSERT INTO movies_genres (id_genre, id_movie)
+VALUES
+(8,1),
+(4,1),
+(8,2),
+(9,2),
+(3,3),
+(12,3),
+(1,4),
+(7,4),
+(11,5),
+(17,5),
+(2,6),
+(15,6),
+(1,7),
+(17,7),
+(1,8),
+(15,8),
+(7,9),
+(17,9),
+(1,10),
+(15,10),
+(1,11),
+(2,11),
+(11,12),
+(11,13),
+(11,14),
+(11,15),
+(4,16),
+(5,16),
+(1,17),
+(17,17),
+(2,18),
+(4,18),
+(1,19),
+(17,19),
+(18,20),
+(1,20);
