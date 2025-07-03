18414c03aa970a6449be0effdf3ad9705c6801b2
Naldy <mdavindarinaldy@gmail.com>
2025-07-03 14:24:38 +0700

    feat: add new struct to handle new movie data

--- dto/movie.go
+++ dto/movie.go
@@ -16,6 +16,19 @@ type Movie struct {
 	Casts       string    `json:"casts" db:"casts"`
 }
 
+type NewMovie struct {
+	Title       string    `json:"title" db:"title"`
+	Synopsis    string    `json:"synopsis" db:"synopsis"`
+	ReleaseDate time.Time `json:"releaseDate" db:"release_date"`
+	Price       float64   `json:"price" db:"price"`
+	Runtime     int       `json:"runtime" db:"runtime"`
+	Poster      string    `json:"poster" db:"poster"`
+	Backdrop    string    `json:"backdrop" db:"backdrop"`
+	Genres      string    `json:"genres" db:"genres"`
+	Directors   string    `json:"directors" db:"directors"`
+	Casts       string    `json:"casts" db:"casts"`
+}
+
 type SubData struct {
 	Id   int    `json:"id" db:"id"`
 	Name string `json:"name" db:"name"`
